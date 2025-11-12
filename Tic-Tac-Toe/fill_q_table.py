28e277ac4d8c186282ff82195b1b167ce329cce3
Varya Kern <varya.kern@gmail.com>
2025-11-13 12:16:35 +1300

    Updated list_possible_states.py to generate winning and gameplay states separately; adjusted win check logic.

--- Tic-Tac-Toe/fill_q_table.py
+++ Tic-Tac-Toe/fill_q_table.py
@@ -1,7 +1,8 @@
 """This code uses dynamic programming to create and fill a q-table with possible moves and Q-scores for each possible state of tic-tac-toe."""
 import list_possible_states
-possible_states = list_possible_states.return_states()
-winning_states = list_possible_states.return_winning()
+# Retrieving list of possible gameplay and won states from the previous code.
+possible_states, winning_states = list_possible_states.return_states()
+
 q_table = []
 for state in possible_states:
     
--- Tic-Tac-Toe/list_possible_states.py
+++ Tic-Tac-Toe/list_possible_states.py
@@ -17,32 +17,47 @@ def valid_state(naughts, crosses):
     """Checks whether the given tic-tac-toe bitboard represents a valid state of the game."""
     # Each square must be occupied by only one piece
     # So naughts AND crosses must return 0
-    n_0 = naughts.bit_count()
-    n_x = crosses.bit_count()
     if naughts & crosses != 0:
-        return False
+        return 0
     # Number of crosses = number of naughts (or number of naughts + 1)
     # So if no_naughts > no_crosses or no_crosses > no_naughts + 1, invalid state
+    n_0 = naughts.bit_count()
+    n_x = crosses.bit_count()
     if (n_0 > n_x) or (n_x > n_0 + 1):
-        return False
-    # Valid non-final position cannot have a row, column, or diagonal filled by one symbol
-    # So naughts/crosses AND any winning state cannot return the winning state
-    # I.e. naughts/crosses cannot have a winning position filled
-    for winning_position in winning_positions:
-        if ((naughts & winning_position == winning_position) and (n_0 != n_x)) or ((crosses & winning_position == winning_position) and (n_x != n_0 + 1)):
-            return False
-    return True
+        return 0
+    # Count number of 'wins' (filled rows, columns, or diagonals) for naughts and for crosses
+    x_wins = any((crosses & winning_pos) == winning_pos for winning_pos in winning_positions)
+    o_wins = any((naughts & winning_pos) == winning_pos for winning_pos in winning_positions)
+    # For a valid crosses win, number of crosses should = no. naughts + 1 
+    if x_wins and n_x != n_0 + 1:
+        return 0
+    # If naughts won, no. of naughts should = no. of crosses
+    if o_wins and n_0 != n_x:
+        return 0
+    # Remaining wins must be valid
+    if x_wins or o_wins:
+        return 2  
+    # Cannot be a win so must be a valid game state
+    return 1
 
 def return_states():
-    """Returns all possible valid states of tic-tac-toe."""
-    possible_states = []
+    """Returns all possible valid winning and gameplay states of tic-tac-toe."""
+    gameplay_states = []
+    winning_states = []
     # The greatest number that a naughts position can correspond to is 480 (111100000)
     # For a crosses position this is 496 (111110000)
     for naughts in range(0, 481):
         for crosses in range(0,497):
-            if valid_state(naughts, crosses):
-                possible_states.append((naughts, crosses))
-    return possible_states
+            result = valid_state(naughts, crosses)
+            if result == 1:
+                gameplay_states.append((naughts, crosses))
+            elif result == 2:
+                winning_states.append((naughts, crosses))
+    return (gameplay_states, winning_states)
+
+gameplay_states, winning_states = return_states()
+print(len(gameplay_states))
+print(len(winning_states))
 
 end_time = time.perf_counter()
 
@@ -50,6 +65,7 @@ print(end_time - start_time)
 
 # 4536 valid non-winning states 
 # 5478 valid states including winning states
+# So 942 winning states
 
 
 
